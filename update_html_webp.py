41ece8dfe89f411cdd1eb315be956da59a90e803
piradioactive312-oss <piradioactive312@gmail.com>
2026-07-13 17:22:51 -0300

    Delete update_html_webp.py

--- update_html_webp.py
+++
@@ -1,9 +0,0 @@
-import glob
-
-html_files = glob.glob('*.html')
-for f in html_files:
-    content = open(f, encoding='utf-8').read()
-    # Replace pngs with webp for the 5 optimized files
-    for name in ['menu-drinks', 'menu-main-dishes', 'menu-sandwiches', 'menu-side-dishes', 'menu-starters']:
-        content = content.replace(f'{name}.png', f'{name}.webp')
-    open(f, 'w', encoding='utf-8').write(content)
